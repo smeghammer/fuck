44fe716107213aa4a9d0a97d20a8c2576a683550
Silas <silas@thecoven.co.uk>
2022-02-17 18:52:16 +0000

    initial commit

---
+++ src/README.md
@@ -0,0 +1,3 @@
+# FUCK!!
+
+The map marker header need to be the name specified, therwis the map doesn't load.
---
+++ src/acs/compile.bat
@@ -0,0 +1,3 @@
+REM replace path with your ACC path:
+D:\Games\Doom\Utils\ACC\acc.exe -i D:\Games\Doom\Utils\ACC\ACC %1 %1
+pause
